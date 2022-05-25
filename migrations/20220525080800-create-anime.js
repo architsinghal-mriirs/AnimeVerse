fb161526a40b15f33e6b1fd666ba8545f1d554cf
archit singhal <zyphergiest@pop-os.localdomain>
2022-05-25 13:40:19 +0530

    Added the Anime SQL Model

---
+++ migrations/20220525080800-create-anime.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Animes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique : true
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      episodeCount: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      studio: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Animes');
+  }
+};
